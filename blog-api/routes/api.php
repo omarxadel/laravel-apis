97ca4f8dbe2056a11dfe72b70d95aa490b7668ff
Omar Adel <48330931+omarxadel@users.noreply.github.com>
2021-09-13 23:59:58 +0200

    Update api.php

--- blog-api/routes/api.php
+++ blog-api/routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CommentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('articles', [ArticleController::class, 'index']);
+Route::get('articles/{id}', [ArticleController::class, 'show']);
+Route::post('articles', [ArticleController::class, 'store']);
+Route::put('articles/{article}', [ArticleController::class, 'update']);
+Route::delete('articles/{article}', [ArticleController::class, 'delete']);
+
+Route::get('profile/{id}', [ArticleController::class, 'user']);
+
+Route::get('users', [UserController::class, 'index']);
+Route::get('users/{user}', [UserController::class, 'show']);
+Route::post('users', [UserController::class, 'store']);
+Route::put('users/{user}', [UserController::class, 'update']);
+Route::delete('users/{user}', [UserController::class, 'delete']);
+
+Route::get('articles/{id}/comments', [CommentController::class, 'show']);
+Route::post('articles/{id}/comments', [CommentController::class, 'store']);
+Route::put('articles/{id}/comments', [CommentController::class, 'update']);
+Route::delete('articles/{id}/comments', [CommentController::class, 'delete']);
